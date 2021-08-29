5f86ea5d8fe1794ae890eebd95431269cda1b104
Diana Lorena <31518102+Dylorena@users.noreply.github.com>
2021-08-29 08:09:35 -0300

    Exercicios 01, 02 e 03

---
+++ tarefa012/ex01_02_03_04/veterinario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013
+{
+    class Veterinario
+    {
+        public string Examinar(Animal animal)
+        {
+            return $"Examinou e o animal fez {animal.EmitirSom()}";
+        }
+    }
+}
